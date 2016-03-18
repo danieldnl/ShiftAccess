0c77549eeaada01103e52a6b021d05b6ccf4c13c
danieldnl <daniel.c.miranda@terra.com.br>
2016-03-18 01:27:56 -0300

    primeiro

---
+++ ShiftAccess/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ShiftAccess
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.lblTravar = new System.Windows.Forms.Label();
+            this.lblDestravar = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTravar
+            // 
+            this.lblTravar.AllowDrop = true;
+            this.lblTravar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTravar.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTravar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblTravar.Location = new System.Drawing.Point(76, 33);
+            this.lblTravar.Name = "lblTravar";
+            this.lblTravar.Size = new System.Drawing.Size(359, 63);
+            this.lblTravar.TabIndex = 0;
+            this.lblTravar.Text = "Travar";
+            this.lblTravar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTravar.DragDrop += new System.Windows.Forms.DragEventHandler(this.lblTravar_DragDrop);
+            this.lblTravar.DragEnter += new System.Windows.Forms.DragEventHandler(this.lblTravar_DragEnter);
+            this.lblTravar.DragLeave += new System.EventHandler(this.lblTravar_DragLeave);
+            // 
+            // lblDestravar
+            // 
+            this.lblDestravar.AllowDrop = true;
+            this.lblDestravar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDestravar.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDestravar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblDestravar.Location = new System.Drawing.Point(76, 141);
+            this.lblDestravar.Name = "lblDestravar";
+            this.lblDestravar.Size = new System.Drawing.Size(359, 63);
+            this.lblDestravar.TabIndex = 1;
+            this.lblDestravar.Text = "Destravar";
+            this.lblDestravar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDestravar.DragDrop += new System.Windows.Forms.DragEventHandler(this.lblDestravar_DragDrop);
+            this.lblDestravar.DragEnter += new System.Windows.Forms.DragEventHandler(this.lblDestravar_DragEnter);
+            this.lblDestravar.DragLeave += new System.EventHandler(this.lblDestravar_DragLeave);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblResult.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(0, 232);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(512, 30);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "label1";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 279);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblDestravar);
+            this.Controls.Add(this.lblTravar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ShiftAccess | Trava ou destrava o uso da tecla shift no MsAccess";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTravar;
+        private System.Windows.Forms.Label lblDestravar;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ ShiftAccess/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.Office.Interop.Access.Dao;
+
+namespace ShiftAccess
+{
+    public partial class frmPrincipal : System.Windows.Forms.Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void lblTravar_DragEnter(object sender, DragEventArgs e)
+        {
+            lblTravar.BackColor = SystemColors.GradientInactiveCaption;
+            EvDragEnter(e);
+        }
+
+        private void lblTravar_DragDrop(object sender, DragEventArgs e)
+        {
+            AllowByPassKey(false, e);
+            lblTravar.BackColor = SystemColors.Control;
+        }
+
+        private void EvDragEnter(DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void AllowByPassKey(bool valor, DragEventArgs e)
+        {
+            string[] caminho = (string[])(e.Data.GetData(DataFormats.FileDrop));
+            var dbe = new DBEngine();
+            Database db = null;
+            try
+            {            
+                db = dbe.OpenDatabase(caminho[0]);
+                db.Properties.Delete("AllowBypassKey");   
+            }
+            catch (Exception err)
+            {
+                if (err.StackTrace.Contains("Access.Dao.Properties.Delete")== false)
+                    MessageBox.Show(err.ToString(),null,MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            finally
+            {  
+                if (db != null)
+                {                 
+                    var prop = db.CreateProperty("AllowByPassKey", 1, valor);
+                    db.Properties.Append(prop);
+                    if (valor == false)
+                    {
+                        lblResult.BackColor = SystemColors.GradientInactiveCaption;
+                        lblResult.Text = "\"" + caminho[0] + "\"" + " travado!";
+                    }
+                    else if (valor == true)
+                    {
+                        lblResult.BackColor = SystemColors.GradientInactiveCaption;
+                        lblResult.Text ="\"" + caminho[0] + "\"" + " destravado!";
+                    }
+
+                    db.Close();
+                }      
+            }
+        }
+
+        private void lblDestravar_DragEnter(object sender, DragEventArgs e)
+        {
+            lblDestravar.BackColor = SystemColors.GradientInactiveCaption;
+            EvDragEnter(e);
+        }
+
+        private void lblDestravar_DragDrop(object sender, DragEventArgs e)
+        {
+            AllowByPassKey(true, e);
+            lblDestravar.BackColor = SystemColors.Control;
+        }
+
+        private void lblTravar_DragLeave(object sender, EventArgs e)
+        {
+            lblTravar.BackColor = SystemColors.Control;
+        }
+
+        private void lblDestravar_DragLeave(object sender, EventArgs e)
+        {
+            lblDestravar.BackColor = SystemColors.Control;
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            lblResult.Text = "";
+        }
+    }
+}
